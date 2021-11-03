9f72d73a10d619032b0202e27ab615ed5d8941d4
RITE-X <re.ritex@gmail.com>
2021-11-03 18:52:15 +0200

    Added several methods for writing and reading length of encrypted data from image

--- BitSY/Stenographer.cs
+++ BitSY/Stenographer.cs
@@ -1,47 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BitSY
+﻿namespace BitSY
 {
     internal static class Stenographer
     {
-        public static byte[] WriteBitesInBitmap(bool[] content, byte[] bitmap)
+        public static void WriteBitsInBitmap(bool[] content, ref byte[] bitmap)
         {
-            byte[] newTarget = (byte[])bitmap.Clone();
-
             var iterator = 0;
-            for (var i = 54; i < newTarget.Length && iterator != content.Length; i++)
+            for (var i = 58; i < bitmap.Length && iterator != content.Length; i++)
             {
                 var color = Convert.ByteToBits(bitmap[i]);
-                for (var j = color.Length - 2; j < color.Length; j++)
+                for (var j = 0; j < 2; j++)
                 {
                     color[j] = content[iterator];
                     iterator++;
                 }
 
-                newTarget[i] = Convert.BitsToByte(color);
+                bitmap[i] = Convert.BitsToByte(color);
             }
-
-            return newTarget;
         }
 
-        public static void ReadBitesFromBitmap(byte[] bitmap, int count)
+        public static bool[] ReadBitsFromBitmap(byte[] bitmap, int count)
         {
-
             var myWord = new bool[count];
-            int iterator = 0;
-            for (var i = 54; i < count / 2 + 54; i++)
+            var iterator = 0;
+            for (var i = 58; i < count / 2 + 58; i++)
             {
                 var color = Convert.ByteToBits(bitmap[i]);
-                for (var j = color.Length - 2; j < color.Length; j++)
+                for (var j = 0; j < 2; j++)
                 {
                     myWord[iterator] = color[j];
                     iterator++;
                 }
             }
+
+            return myWord;
+        }
+
+        public static byte[] WriteTextLenghtToKey(byte[] key, int lenght)
+        {
+            var newKey = (byte[])key.Clone();
+            var lenghtInBytes = Convert.IntToBytes(lenght);
+
+            for (int i = key.Length - lenghtInBytes.Length, j = 0; i < key.Length; i++, j++)
+            {
+                newKey[i] = lenghtInBytes[j];
+            }
+
+            return newKey;
+        }
+
+        public static int ReadTextLenghtFromKey(byte[] key)
+        {
+            var lenghtInBytes = new byte[4];
+            for (int i = key.Length - 4, j = 0; i < key.Length; i++, j++)
+            {
+                lenghtInBytes[j] = key[i];
+            }
+
+            return Convert.BytesToInt(lenghtInBytes);
+        }
+
+        public static void WriteTextLenghtToBitmap(ref byte[] bitmap, int lenght)
+        {
+            var lenghtInBytes = Convert.IntToBytes(lenght);
+
+            for (int i = 54, j = 0; i < 58; i++, j++)
+            {
+                bitmap[i] = lenghtInBytes[j];
+            }
+        }
+
+        public static int ReadTextLenghtFromBitmap(byte[] bitmap)
+        {
+            var lenghtInBytes = new byte[4];
+            for (int i = 54, j = 0; i < 58; i++, j++)
+            {
+                lenghtInBytes[j] = bitmap[i];
+            }
+
+            return Convert.BytesToInt(lenghtInBytes);
         }
     }
 }
