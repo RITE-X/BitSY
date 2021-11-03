042c3dd1193f47d573e79cfcf6e52887ac6d9131
RITE-X <re.ritex@gmail.com>
2021-11-03 18:54:07 +0200

    Added methods for converting types

--- BitSY/Convert.cs
+++ BitSY/Convert.cs
@@ -1,69 +1,104 @@
 ﻿using System;
 using System.Collections;
 
-
 namespace BitSY
 {
     internal static class Convert
     {
-        public static byte BitsToByte(BitArray scr)
+        public static byte BitsToByte(BitArray item)
         {
             byte num = 0;
-            for (var i = 0; i < scr.Count; i++)
-                if (scr[i] == true)
+            for (var i = 0; i < item.Count; i++)
+                if (item[i])
+                {
                     num += (byte)Math.Pow(2, i);
+                }
+
             return num;
         }
 
-        public static BitArray ByteToBits(byte src)
+        public static BitArray ByteToBits(byte item)
         {
             var bitArray = new BitArray(8);
             for (var i = 0; i < bitArray.Length; i++)
             {
-                var bit = (src >> i & 1) == 1;
+                var bit = (item >> i & 1) == 1;
                 bitArray[i] = bit;
             }
+
             return bitArray;
         }
 
         public static bool[] BytesToBits(byte[] bytes)
         {
-            var textInBites = new bool[bytes.Length * 8];
+            var textInBits = new bool[bytes.Length * 8];
 
             var iterator = 0;
-            foreach (var _byte in bytes)
+            foreach (var @byte in bytes)
             {
-                var character = ByteToBits(_byte);
+                var character = ByteToBits(@byte);
                 for (var j = 0; j < 8; j++)
                 {
-                    textInBites[iterator] = character[j];
+                    textInBits[iterator] = character[j];
                     iterator++;
                 }
             }
 
-            return textInBites;
+            return textInBits;
         }
 
-        public static byte[] BitsToBytes(bool[] bites)
+        public static byte[] BitsToBytes(bool[] bits)
         {
-            byte[] bytes = new byte[bites.Length/8];
+            var bytes = new byte[bits.Length / 8];
 
 
-            int g = 0;
-            for (int i = 0; i < bytes.Length; i++)
+            var iterator = 0;
+            for (var i = 0; i < bytes.Length; i++)
             {
-                var gf = new BitArray(8);
-                for (int j = 0; j < 8; j++)
+                var nextByte = new BitArray(8);
+                for (var j = 0; j < 8; j++)
                 {
-                    gf[j] = bites[g];
-                    g++;
+                    nextByte[j] = bits[iterator]; 
+                    iterator++;
                 }
 
-                bytes[i] = Convert.BitsToByte(gf);
+                bytes[i] = BitsToByte(nextByte);
             }
+
             return bytes;
         }
 
+        public static int BytesToInt(byte[] bytes)
+        {
+            if (bytes.Length > 4) throw new ArgumentOutOfRangeException(nameof(bytes));
+            return BitConverter.ToInt32(bytes);
+        }
+
+        public static byte[] IntToBytes(int integer)
+        {
+            return BitConverter.GetBytes(integer);
+        }
+
+        public static string BytesToString(byte[] content)
+        {
+            var str = new char[content.Length];
+            for (var i = 0; i < str.Length; i++)
+            {
+                str[i] = (char)content[i];
+            }
+
+            return new string(str);
+        }
+
+        public static byte[] StringToBytes(string str)
+        {
+            var content = new byte[str.Length];
+            for (var i = 0; i < str.Length; i++)
+            {
+                content[i] = (byte)str[i];
+            }
+
+            return content;
+        }
     }
 }
-
